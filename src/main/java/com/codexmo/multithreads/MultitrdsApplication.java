1b1bc80673608997120d95dedcda5eb6f4f55ded
Thilan Madusanka <thilan87189@gmail.com>
2024-04-19 11:43:24 +0530

    multithread first commit

---
+++ src/main/java/com/codexmo/multithreads/MultitrdsApplication.java
@@ -0,0 +1,13 @@
+package com.codexmo.multitrds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitrdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultitrdsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/codexmo/multithreads/enitiy/User.java
@@ -0,0 +1,30 @@
+package com.codexmo.multitrds.enitiy;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+
+
+}
